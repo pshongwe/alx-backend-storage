66a7465862461a8611c3ff50ce90f568c4774c99
pshongwe <piboman@yahoo.com>
2024-06-17 03:36:07 +0000

    update

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,38 +1,37 @@
 -- Create the stored procedure to compute average 
 -- weighted score for all users
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser()
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
-    DECLARE total_weighted_score FLOAT;
-    DECLARE total_weight INT;
-    DECLARE done INT DEFAULT 0;
-    DECLARE userId INT;
-    DECLARE userCursor CURSOR FOR SELECT id FROM users;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+    ALTER TABLE users ADD temp_total_weighted_score INT NOT NULL;
+    ALTER TABLE users ADD temp_total_weight INT NOT NULL;
 
-    OPEN userCursor;
-    read_loop: LOOP
-        FETCH userCursor INTO userId;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
+    UPDATE users
+        SET temp_total_weighted_score = (
+            SELECT SUM(corrections.score * projects.weight)
+            FROM corrections
+                INNER JOIN projects
+                    ON corrections.project_id = projects.id
+            WHERE corrections.user_id = users.id
+        );
 
-        SELECT SUM(c.score * p.weight), SUM(p.weight)
-        INTO total_weighted_score, total_weight
-        FROM corrections c
-        JOIN projects p ON c.project_id = p.id
-        WHERE c.user_id = userId;
+    UPDATE users
+        SET temp_total_weight = (
+            SELECT SUM(projects.weight)
+            FROM corrections
+                INNER JOIN projects
+                    ON corrections.project_id = projects.id
+            WHERE corrections.user_id = users.id
+        );
 
-        IF total_weight > 0 THEN
-            SET total_weighted_score = total_weighted_score / total_weight;
-        ELSE
-            SET total_weighted_score = 0;
-        END IF;
+    UPDATE users
+        SET users.average_score = IF(users.temp_total_weight = 0, 0, users.temp_total_weighted_score / users.temp_total_weight);
 
-        UPDATE users
-        SET average_score = total_weighted_score
-        WHERE id = userId;
-    END LOOP;
-
-    CLOSE userCursor;
+    ALTER TABLE users
+        DROP COLUMN temp_total_weighted_score;
+    ALTER TABLE users
+        DROP COLUMN temp_total_weight;
 END $$
+
 DELIMITER ;
