9f7f98c11f05ab8107c1f8db9c3f86823b4a498e
pshongwe <piboman@yahoo.com>
2024-06-19 18:38:12 +0000

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,21 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn: Optional[Callable] = None)
+    -> Union[str, bytes, int, float, None]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, fn=int)
+
 
 if __name__ == "__main__":
     cache = Cache()
