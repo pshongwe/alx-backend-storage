51401104de5556660d1265411ba1a5a33fb2016f
pshongwe <piboman@yahoo.com>
2024-06-19 19:39:14 +0000

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,43 +11,28 @@ from typing import Callable
 cache_client = redis.Redis()
 
 
-def track_and_cache(expiration: int = 10):
-    """Decorator to track URL access and cache the response with expiration."""
+def cache_page(expiration: int = 10):
+    """Decorator to cache the page content and track URL access."""
     def decorator(func: Callable) -> Callable:
+        """deco"""
         @wraps(func)
         def wrapper(url: str, *args, **kwargs) -> str:
-            access_count_key = f"count:{url}"
-            cache_client.incr(access_count_key)
-
-            cached_content = cache_client.get(f"count:{url}")
+            """wrapper"""
+            count_key = f"count:{url}"
+            cache_client.incr(count_key)
+            cached_content = cache_client.get(url)
             if cached_content:
                 return cached_content.decode('utf-8')
-
             response_content = func(url, *args, **kwargs)
-            cache_client.setex(f"count:{url}",
-                               expiration, response_content)
+            cache_client.setex(url, expiration, response_content)
 
             return response_content
         return wrapper
     return decorator
 
 
-@track_and_cache()
+@cache_page()
 def get_page(url: str) -> str:
     """Fetch the HTML content of the specified URL."""
     response = requests.get(url)
     return response.text
-
-
-if __name__ == "__main__":
-    test_url = (
-        "http://slowwly.robertomurray.co.uk/delay/5000/url/"
-        "http://www.example.com"
-    )
-
-    for _ in range(3):
-        content = get_page(test_url)
-        print(content[:100])  # Print the first 100 characters of the content
-
-    access_count = cache_client.get(f"access_count:{test_url}")
-    print(f"Access count for {test_url}: {access_count.decode('utf-8')}")
