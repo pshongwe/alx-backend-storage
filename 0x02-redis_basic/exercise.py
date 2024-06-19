8b4b871e93160cd4be084b8e7ec7bb10ee88ca35
pshongwe <piboman@yahoo.com>
2024-06-19 19:02:17 +0000

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -82,16 +82,3 @@ class Cache:
 
     def get_int(self, key: str) -> Optional[int]:
         return self.get(key, fn=int)
-
-
-if __name__ == "__main__":
-    cache = Cache()
-
-    s1 = cache.store("foo")
-    print(s1)
-    s2 = cache.store("bar")
-    print(s2)
-    s3 = cache.store(42)
-    print(s3)
-
-    replay(cache.store)
